a10ee1e24f70945892be94bdf60b597ef2904c53
Elvis Jr Crego <elvis.crego@gmail.com>
2019-01-09 21:59:24 -0500

    Incluyendo observaciones en indisponibilidades

--- GestionRestaurants/Controllers/IndisponibilidadesController.cs
+++ GestionRestaurants/Controllers/IndisponibilidadesController.cs
@@ -55,7 +55,7 @@ namespace GestionRestaurants.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Nuevo([Bind("Id,FechaInicio,FechaFin,RestaurantId")] IndisponibilidadRestaurant indisponibilidad)
+        public async Task<IActionResult> Nuevo([Bind("Id,FechaInicio,FechaFin,RestaurantId,Observaciones")] IndisponibilidadRestaurant indisponibilidad)
         {
             if (ModelState.IsValid)
             {
@@ -89,7 +89,7 @@ namespace GestionRestaurants.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Editar(int id, [Bind("Id,FechaInicio,FechaFin,RestaurantId")] IndisponibilidadRestaurant indisponibilidad)
+        public async Task<IActionResult> Editar(int id, [Bind("Id,FechaInicio,FechaFin,RestaurantId,Observaciones")] IndisponibilidadRestaurant indisponibilidad)
         {
             if (id != indisponibilidad.Id)
             {
